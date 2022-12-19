042ddba9a5142eaf2d629195c994dd18f711c0a0
Lucy Poulton <lucy@lucypoulton.net>
2022-12-19 23:04:12 +0000

    javadocs for builtin, unset set

--- pronouns-api/src/main/java/net/lucypoulton/pronouns/api/PronounSet.java
+++ pronouns-api/src/main/java/net/lucypoulton/pronouns/api/PronounSet.java
@@ -72,17 +72,46 @@ public interface PronounSet {
         return new SimplePronounSet(subjective, objective, possessiveAdj, possessive, reflexive, plural);
     }
 
+    /**
+     * A collection of builtin sets.
+     */
     class Builtins {
         private Builtins() {}
 
+        /**
+         * The they/them pronoun set. Makes use of the more common plural "themselves" reflexive.
+         */
         public static final PronounSet THEY = PronounSet.from("they", "them", "their", "theirs", "themselves", true);
+
+        /**
+         * The he/him pronoun set.
+         */
         public static final PronounSet HE = PronounSet.from("he", "him", "his", "his", "himself", false);
+
+        /**
+         * The she/her pronoun set.
+         */
         public static final PronounSet SHE = PronounSet.from("she", "her", "her", "hers", "herself", false);
 
+        /**
+         * A special set, proxied to they/them, that indicates any pronouns are acceptable.
+         */
         public static final PronounSet ANY = new SpecialPronounSet(THEY, "Any");
+
+        /**
+         * A special set, proxied to they/them, that indicates to ask for the subject's pronouns..
+         */
         public static final PronounSet ASK = new SpecialPronounSet(THEY, "Ask");
+
+        /**
+         * A special set, proxied to they/them, that indicates pronouns have not been set.
+         */
+        public static final PronounSet UNSET = new SpecialPronounSet(THEY, "Unset");
 
     }
 
+    /**
+     * A supplier for all pronound defined in {@link Builtins}, except for {@link Builtins#UNSET}.
+     */
     PronounSupplier builtins = () -> Set.of(Builtins.THEY, Builtins.HE, Builtins.SHE, Builtins.ANY, Builtins.ASK);
 }
